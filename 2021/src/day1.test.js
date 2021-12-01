d6ab8015a2be84807504f6283f1deee0f9cd594b
Michael Weilbächer <nakih@micbook.fritz.box>
2021-12-02 00:46:36 +0100

    day one

--- 2021/src/day1.test.js
+++ 2021/src/day1.test.js
@@ -1,17 +1,44 @@
-const { measureDepth } = require("./day1");
-test("measureDepth is measuring correctly", () => {
-  const depth = measureDepth([
+const { measureDepthPartA, measureDepthPartB } = require("./day1");
+test("measureDepthPartA is measuring correctly", () => {
+  const depth = measureDepthPartA([
     199, 200, 208, 210, 200, 207, 240, 269, 260, 263,
   ]);
   expect(depth).toEqual(7);
 });
 
-test("measureDepth is measuring correctly with empty sonar", () => {
-  const depth = measureDepth([]);
+test("measureDepthPartA is measuring correctly with empty sonar", () => {
+  const depth = measureDepthPartA([]);
   expect(depth).toEqual(0);
 });
 
-test("measureDepth is measuring correctly with only 1 datapoint", () => {
-  const depth = measureDepth([193]);
+test("measureDepthPartA is measuring correctly with undefined sonar", () => {
+  const depth = measureDepthPartA();
+  expect(depth).toEqual(0);
+});
+
+test("measureDepthPartA is measuring correctly with only 1 datapoint", () => {
+  const depth = measureDepthPartA([193]);
+  expect(depth).toEqual(0);
+});
+
+test("measureDepthPartB is measuring correctly", () => {
+  const depth = measureDepthPartB([
+    199, 200, 208, 210, 200, 207, 240, 269, 260, 263,
+  ]);
+  expect(depth).toEqual(5);
+});
+
+test("measureDepthPartB is measuring correctly with empty sonar", () => {
+  const depth = measureDepthPartB([]);
+  expect(depth).toEqual(0);
+});
+
+test("measureDepthPartA is measuring correctly with undefined sonar", () => {
+  const depth = measureDepthPartB();
+  expect(depth).toEqual(0);
+});
+
+test("measureDepthPartA is measuring correctly with less than 4 datapoints", () => {
+  const depth = measureDepthPartB([193, 123, 124]);
   expect(depth).toEqual(0);
 });
